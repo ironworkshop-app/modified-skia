99c297c074c24846bdf6c07924990afa6871cd46
Luo Weibin <cykit126@gmail.com>
2022-10-05 16:00:29 +0800

    添加编译脚本。

---
+++ apple_tools/archive_iOS_Debug.sh
@@ -0,0 +1,12 @@
+DST_DIR=$DEVENV/aiSDK/prebuilt/iOS/lib/Debug
+
+cp out/iOS_Debug/libharfbuzz.a $DST_DIR/libharfbuzz.a
+cp out/iOS_Debug/libicu.a $DST_DIR/libicu.a  
+cp out/iOS_Debug/libparticles.a $DST_DIR/libparticles.a 
+cp out/iOS_Debug/libpathkit.a $DST_DIR/libpathkit.a
+cp out/iOS_Debug/libskia.a $DST_DIR/libskia.a 
+cp out/iOS_Debug/libskparagraph.a $DST_DIR/libskparagraph.a 
+cp out/iOS_Debug/libskresources.a $DST_DIR/libskresources.a 
+cp out/iOS_Debug/libskshaper.a $DST_DIR/libskshaper.a 
+cp out/iOS_Debug/libskunicode.a $DST_DIR/libskunicode.a 
+cp out/iOS_Debug/libsktext.a $DST_DIR/libsktext.a 
