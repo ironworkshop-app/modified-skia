5cd0ed0bf81eba601341888c8995143852eecd21
Luo Weibin <cykit126@gmail.com>
2023-05-10 15:20:45 +0800

    添加Windows编译脚本。

---
+++ tools_aiSDK/Windows/build_Windows_Debug.ps1
@@ -0,0 +1,40 @@
+
+$DIR = $PSScriptRoot
+$GN = "$DIR\..\..\bin\gn.exe"
+$OUTPUT_DIR = "out/Windows_Debug"
+
+mkdir -Force $OUTPUT_DIR | Out-Null
+
+Write-Output "DIR:$DIR"
+Write-Output "OUTPUT_DIR:$OUTPUT_DIR"
+
+& $GN gen $OUTPUT_DIR --args='
+    is_official_build = false 
+    skia_use_system_icu = false 
+    skia_use_system_harfbuzz = false 
+    skia_use_libwebp_decode = false 
+    skia_use_libwebp_encode = false
+    skia_use_libpng_encode = false 
+    skia_use_libpng_decode = false 
+    skia_use_libjpeg_turbo_encode = false 
+    skia_use_libjpeg_turbo_decode = false 
+    skia_use_libgifcodec = false 
+    skia_use_gl = false 
+    skia_enable_svg = false 
+    skia_enable_skottie = false 
+    skia_enable_pdf = false 
+    skia_use_piex = false 
+    skia_use_expat = false
+    skia_use_metal = false
+    skia_use_direct3d = true 
+    skia_enable_tools = false
+    paragraph_tests_enabled = false 
+    paragraph_gms_enabled = false 
+    paragraph_bench_enabled = false 
+    target_cpu = \"x64\"
+    cc = \"clang\"
+    cxx = \"clang++\"
+    clang_win = \"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\"
+'
+
+ninja -C $OUTPUT_DIR
