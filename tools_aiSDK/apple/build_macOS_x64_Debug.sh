bbd7da6666caed9cc7e695bef42ca39d5639b17a
Luo Weibin <cykit126@gmail.com>
2023-05-29 10:47:46 +0800

    完善macOS x64编译脚本。

---
+++ tools_aiSDK/apple/build_macOS_x64_Debug.sh
@@ -0,0 +1,71 @@
+DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+SRC_DIR=$DIR/../..
+GN=$SRC_DIR/bin/gn
+OUTPUT_DIR=$SRC_DIR/out/macOS_x64_Debug
+DEST_DIR=$DEVENV/aiSDK/prebuilt/macOS_x64_Debug
+DEST_INCLUDE_DIR=$DEST_DIR/include
+SKIA_DIR=$DEST_DIR/extra/skia
+THIRD_PARTY=$OUTPUT_DIR/../../third_party
+
+mkdir -p $OUTPUT_DIR
+
+echo "DIR:$DIR"
+echo "OUTPUT_DIR:$OUTPUT_DIR"
+
+$DIR/../../bin/gn gen out/macOS_x64_Debug --args='
+is_official_build = false 
+skia_use_system_icu = false 
+skia_use_system_harfbuzz = false 
+skia_use_system_zlib = false 
+skia_use_system_libwebp = false
+skia_use_system_libpng = false
+skia_use_system_libjpeg_turbo = false
+skia_use_system_expat = false
+skia_use_libwebp_encode = true
+skia_use_gl = true 
+skia_enable_svg = true 
+skia_enable_pdf = false
+skia_use_metal = false
+skia_use_direct3d = false 
+skia_enable_tools = false
+paragraph_tests_enabled = false 
+paragraph_gms_enabled = false 
+paragraph_bench_enabled = false 
+target_cpu = "x86_64"
+macos_min_target = "11.0"
+'
+
+ninja -C out/macOS_x64_Debug
+
+mkdir -p $DEST_DIR/lib
+cp $OUTPUT_DIR/*.a $DEST_DIR/lib
+mv $DEVENV/aiSDK/prebuilt/macOS_x64_Debug/lib/libzlib.a $DEST_DIR/lib/libz.a
+
+mkdir -p $SKIA_DIR/include
+rsync -r --include="*/" --include="*.h" --exclude="*" $SRC_DIR/include $SKIA_DIR
+
+mkdir -p $SKIA_DIR/src/base
+rsync -r --include="*/" --include="*.h" --exclude="*" $SRC_DIR/src/base $SKIA_DIR/src 
+
+mkdir -p $SKIA_DIR/src/core
+rsync -r --include="*/" --include="*.h" --exclude="*" $SRC_DIR/src/core $SKIA_DIR/src 
+
+mkdir -p $SKIA_DIR/modules/skparagraph/include
+rsync -r --include="*/" --include="*.h" --exclude="*" $SRC_DIR/modules/skparagraph/include $SKIA_DIR/modules/skparagraph 
+
+# zlib
+mkdir -p $DEST_INCLUDE_DIR
+cp $THIRD_PARTY/externals/zlib/zlib.h $DEST_INCLUDE_DIR/zlib.h
+cp $THIRD_PARTY/externals/zlib/zconf.h $DEST_INCLUDE_DIR/zconf.h
+
+# libpng
+cp $THIRD_PARTY/externals/libpng/png.h $DEST_INCLUDE_DIR/png.h
+cp $THIRD_PARTY/externals/libpng/pngconf.h $DEST_INCLUDE_DIR/pngconf.h
+cp $THIRD_PARTY/libpng/pnglibconf.h $DEST_INCLUDE_DIR/pnglibconf.h
+
+# icu
+rsync -r --include="*/" --include="*.h" --exclude="*" $THIRD_PARTY/externals/icu/source/common/unicode $DEST_INCLUDE_DIR
+
+
+
